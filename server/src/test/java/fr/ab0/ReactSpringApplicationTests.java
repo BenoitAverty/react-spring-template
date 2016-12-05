dad938da6234e8fb4dcd62c44ac6650cea290914
Benoit Averty <benoit.averty@gmail.com>
2016-12-05 16:28:24 +0100

    Fix tests

--- server/src/test/java/fr/ab0/ReactSpringApplicationTests.java
+++ server/src/test/java/fr/ab0/ReactSpringApplicationTests.java
@@ -1,12 +1,13 @@
 package fr.ab0;
 
+import fr.ab0.reactspring.ReactSpringApplication;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
-@SpringBootTest
+@SpringBootTest(classes = ReactSpringApplication.class)
 public class ReactSpringApplicationTests {
 
 	@Test
