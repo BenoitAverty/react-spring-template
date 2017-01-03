cf6df75b854970e8fca2b7c38e8e5683134f8808
Benoit Averty <benoit.averty@gmail.com>
2017-01-03 16:38:26 +0100

    Add the client to fat jar during build, and serve index.html for each route

--- server/src/main/java/fr/ab0/reactspring/ReactSpringApplication.java
+++ server/src/main/java/fr/ab0/reactspring/ReactSpringApplication.java
@@ -3,6 +3,9 @@ package fr.ab0.reactspring;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * Bootstraping of the java app
+ */
 @SpringBootApplication
 public class ReactSpringApplication {
 
